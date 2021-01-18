c1d50695a8fd7bfd87f84a3f32fe7f0bad1d6c6d
brianabajah <abajahbrian@gmail.com>
2021-01-18 13:16:59 -0600

    Side menu fix

--- comics/src/components/Main.js
+++ comics/src/components/Main.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles , useTheme} from '@material-ui/core/styles';
 import Comics from './Comics';
 import Grid from '@material-ui/core/Grid';
 import Carousel from 'react-material-ui-carousel'
 import Paper from '@material-ui/core/Paper';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import Divider from '@material-ui/core/Divider';
+import MenuIcon from '@material-ui/icons/Menu';
+import clsx from 'clsx';
 // import Button from '@material-ui/core/Button';
 
-
+const drawerWidth = 240;
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -32,11 +45,56 @@ const useStyles = makeStyles((theme) => ({
 //         justify:"space-between"
 //       }
 //   }
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }
 }));
 
 
 export default function Main() {
     const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+    
     var items = [
         {
             name: "Random Name #1",
@@ -73,18 +131,51 @@ export default function Main() {
                 </Carousel>
                 </Grid>
 
-{/* Comics menu and Search */}
-                <Grid container item xs={12} spacing={2}
+{/* Comics, menu and Search */}
+                <Grid container item xs={12} spacing={1}
                    direction="row"
-                   justify="space-between"
-                   alignItems="stretch"
+                   justify="flex-start"
+                   alignItems="flex-start"
                    className={classes.panels}
                    >
-                       <Grid container item xs={12} sm={9} spacing={3}
+                       <Grid container item  sm={1} spacing={1}
                             direction="row"
                             justify="center"
                             alignItems="stretch">
-
+                                <IconButton
+                                    color="inherit"
+                                    aria-label="open drawer"
+                                    onClick={handleDrawerOpen}
+                                    edge="start"
+                                    className={clsx(classes.menuButton, open && classes.hide)}
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Drawer
+                                    className={classes.drawer}
+                                    variant="persistent"
+                                    anchor="left"
+                                    open={open}
+                                    classes={{
+                                    paper: classes.drawerPaper,
+                                    }}
+                                >
+                                    <div className={classes.drawerHeader}>
+                                    <IconButton onClick={handleDrawerClose}>
+                                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                                    </IconButton>
+                                    </div>
+                                    <Divider />
+                                    <List>
+                                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                                        <ListItem button key={text}>
+                                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                        <ListItemText primary={text} />
+                                        </ListItem>
+                                    ))}
+                                    </List>
+                                    
+                                </Drawer>
                         </Grid>
 
                        {/* comic panel */}
@@ -96,7 +187,7 @@ export default function Main() {
                             </Grid>
 
                         {/* right panel */}
-                            <Grid item xs={12} sm={3} >
+                            <Grid item xs={12} sm={2} >
                                 <div className={classes.rightpanel}>
                                     Hello World
                                 </div>
